566bf9740aa88c62e4163ba921149c84c1097978
Mark Sherry <marks@sortable.com>
2018-10-03 14:29:54 -0400

    Bitwise `&` to match Java version

--- src/main.rs
+++ src/main.rs
@@ -23,7 +23,7 @@ fn main() {
         let mut output = vec![0; BASE32.decode_len(auction_id.len()).unwrap()];
         auction_id.make_ascii_uppercase();
         let len = BASE32.decode_mut(auction_id.as_bytes(), &mut output).unwrap();
-        let val = Cursor::new(&output[..len]).read_u64::<BigEndian>().unwrap();
+        let val = Cursor::new(&output[..len]).read_u64::<BigEndian>().unwrap() & 0x7fff_ffff_ffff_ffff;
         auction_id.make_ascii_lowercase();
         println!("{} {:>24} {:>24}", auction_id, val, val % args.modulus);
     }
